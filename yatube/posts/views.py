4223018f81a11bc236672e0ca7791fa2549a0970
mdeath.zinoviev@yandex.ru <mdeath.zinoviev@yandex.ru>
2023-01-27 19:16:56 +0300

    Домашняя работа спринт 3 тема 4 урок 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse(f'Посты по группам {slug}')
