bda993891b0dee48168317c9c20c6a90216dcf62
mdeath.zinoviev@yandex.ru <mdeath.zinoviev@yandex.ru>
2023-02-17 16:11:20 +0300

    Спринт3 финалка

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     """Класс модели группы."""
     title = models.CharField(max_length=100)
